c590355a5012bae34d5f013c1e551acd938c0a4b
Thomas Kosa <thomaskosa24@gmail.com>
2019-06-16 09:58:06 -0400

    refactored for cleanliness and modularity

--- updateFolder.py
+++ updateFolder.py
@@ -1,84 +1,72 @@
 import os
 from utilityFns import *
-import tkinter as tk
+from tkinter import *
 from tkinter.filedialog import askdirectory
 
+class Application(Frame):
+    def __init__(self, master=None):
+        super().__init__(master,width=100)
+        self.master = master
+        self.input_frame = InputFrame(self)
+        self.pack()
 
-def gui(src="Path of Src Directory",dst="Path of Destination Directory"):
-
-
-
-    def choose_src():
-        src=askdirectory()
-        top_left.config(text=src)
-
-        #If program has already run but they change src/dst, runbtn will work again
-        run_button.config(text='Run')
-        run_button.config(command = run_btn)
-
-    def choose_dst():
-        global dst
-        dst=askdirectory()
-        middle_left.config(text=dst)
-
-        #If program has already run but they change src/dst, runbtn will work again
-        run_button.config(text='Run')
-        run_button.config(command = run_btn)
-
-    root=tk.Tk()
-    root.title("Update-Folder")
-    top_label=tk.Label(root)
-    top_left=tk.Label(top_label,width=60)
-    top_left.config(text=src,bg="white")
-    top_left.pack(padx=5, side=tk.LEFT)
-
-    top_right=tk.Button(top_label, text='Choose Src Directory', width=25, command=choose_src)
-    top_right.config(text="Choose Src")
-    top_right.pack( side=tk.RIGHT)
-
-    middle_label=tk.Label(root)
-    middle_left=tk.Label(middle_label,width=60)
-    middle_left.config(text=dst,bg="white")
-    middle_left.pack(padx=5, side=tk.LEFT)
-
-    middle_right=tk.Button(middle_label, text='Choose Src Directory', width=25, command=choose_dst)
-    middle_right.config(text="Choose Dst")
-    middle_right.pack(side=tk.RIGHT)
-
-    def run_btn():
-        src=top_left.cget("text")
-        dst=middle_left.cget("text")
-
-        print(src + " " + dst)
-        print(os.path.exists(dst))
-
-        if not os.path.exists(src) or not os.path.exists(dst) :
-            run_button.config(text='Error: path or src does not exist')
-            return
-        if os.path.realpath(dst).find(os.path.realpath(src))!=-1:
-            run_button.config(text='Error: dst is IN src')
+    def run(self):
+        src = self.input_frame.src_row.label.cget("text")
+        dst = self.input_frame.dst_row.label.cget("text")
+        runBtn = self.input_frame.runBtn
+        
+        if not os.path.exists(src) or not os.path.exists(dst):
+            runBtn.config(text='Error: path or src does not exist')
             return
 
-        run_button.config(text='Running')
-        update(src,dst)
-        run_button.config(text='Finished Running. Click to close.')
-        run_button.config(command = lambda: root.destroy())
+        if os.path.realpath(dst).find(os.path.realpath(src)) != -1:
+            runBtn.config(text='Error: dst is IN src')
+            return
 
-    run_button=tk.Button(root, text='Run', width=45, command = run_btn)
+        runBtn.config(text='Running')
+        update(src, dst)
 
-    top_label.pack()
-    middle_label.pack()
-    run_button.pack()
-
-    root.mainloop()
+        runBtn.config(text='Run')
+        runBtn.config(command=self.run)
 
 
-def main():
-    #Default src/dst if you use the program often
-    src = "J:\\T"
-    dst = "C:\\Users\\Thomas\\Desktop\\T\\Update"
-    gui(src,dst)
+
+class InputFrame(Frame):
+    def __init__(self, master):
+        super().__init__(master,width=100)
+
+        self.src_row = SelectDirectoryRow(self, "Src")
+        self.dst_row = SelectDirectoryRow(self, "Dst")
+
+        self.runBtn = Button(self, text="Run", command=master.run)
+        self.runBtn.pack()
+
+        self.pack()
+
+class SelectDirectoryRow(Frame):
+    #A frame with a button and label. On button click, user selects directory, which then appears in label.
+
+    def __init__(self, master, text=""):
+        super().__init__(master,width=100)
+
+        self.label = Label(self, text="", bg="white", width=65)
+        self.label.pack(padx=5, side=LEFT)
+
+        self.button = Button(self, text=text, width=25, command=self.onClick)
+        self.button.pack(side=LEFT)
+
+        self.pack(fill=X)
+
+    def onClick(self):
+        self.label.config(text=askdirectory())
+
+
+
+
 
 if __name__=="__main__":
     # This version is for desktop
-    main()
+    root = Tk()
+    root.title("Update-Folder")
+    app = Application(master=root)
+    root.mainloop()
